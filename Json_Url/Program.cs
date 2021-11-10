a945e2c20435cf6739bddf8d3d494efb44b9ca2f
Adam <adam.gebicki01@gmail.com>
2021-11-10 14:30:26 +0100

    Init. 10.11.2021 Signed-off-by: Adam <adam.gebicki01@gmail.com>

---
+++ Json_Url/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Json_Url
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
